11ed207727e60af1957c4d2545b8bdb4468c90a7
YuTMF <sozdatelmonstra@gmail.com>
2021-10-29 00:14:20 +0500

    Задания 8-10 Самостоятельная работа: вычисления

---
+++ src/Example15.java
@@ -0,0 +1,21 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Example15 {
+
+    /*TODO: 8. Напишите программу для вычисления суммы двух чисел. Оба числа
+       вводятся пользователем. Для вычисления суммы используйте оператор +.*/
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Задание: вычислить сумму двух чисел\nВведите первое число: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = in.nextInt();
+
+        System.out.printf("Сумма введенных чисел: %d", num1 + num2);
+        in.close();
+    }
+}
---
+++ src/Example17.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Example17 {
+    /*TODO: 10. Напишите программу, в которой Пользователь вводит два числа, а
+       программой вычисляется и отображается сумма и разность этих чисел.*/
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Задание: вычислить сумму и разность двух чисел\nВведите первое число: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = in.nextInt();
+
+        System.out.printf("Сумма введенных чисел: %d" +
+                "\nРАзность введенных чисел: %d"
+                , num1 + num2
+                , num1 - num2
+        );
+        in.close();
+    }
+}
